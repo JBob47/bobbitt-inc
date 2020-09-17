c11b8fcc3da403e94483601882113ac4d506d4cc
Justin Bobbitt <justin@justinbobbittdesign.com>
2020-09-17 11:55:01 -0400

    Rough Mockup of Portfolio main page. Gallery images are loaded with CSS background images.

--- Bobbitt_Inc/urls.py
+++ Bobbitt_Inc/urls.py
@@ -23,5 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    path('/portfolio', include('portfolio.urls')),
+    path('portfolio/', include('portfolio.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
